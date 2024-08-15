ec1d9813e6faf86b1ebc2d4609617dd0c2a7babc
BlackieStars <diviprakashpc@gmail.com>
2024-08-15 14:05:35 +0530

    removed deployment code

--- server.js
+++ server.js
@@ -18,23 +18,6 @@ app.use(express.json());
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
-// ===========DEPLOYMENT=================
-  __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-//* means all paths other than our path that we have created i.e. managerroutes, userroutes etc..//
-
-// ===========DEPLOYMENT=================
 
 app.use(notFound);
 app.use(errorHandler); // with this we get error in more structured format.
